f9e4f12121cc0afce26e99a83b19be3891461f59
Dave Torrente <estrada.torrente@gmail.com>
2021-12-09 18:28:51 +0800

    initial setup of blog crud gRPC

---
+++ blog/blog_server/server.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"flag"
+	"fmt"
+	"go-grpc/blog/blogpb"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"time"
+
+	_ "github.com/lib/pq"
+	"google.golang.org/grpc"
+)
+
+type config struct {
+	db struct {
+		dsn string
+	}
+}
+
+type server struct{}
+
+type Blog struct {
+	ID       int    `json:"id"`
+	AuthorID string `json: author_id`
+	Content  string `json: content`
+	Title    string `json: title`
+}
+
+func main() {
+	//if we crash something, we get the file and error number
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	var cfg config
+	flag.StringVar(&cfg.db.dsn, "dsn", "postgres://postgres:root@localhost/go_movies?sslmode=disable", "Postgres connection string")
+	db, err := openDB(cfg)
+	fmt.Println("Connecting to Posgres")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	fmt.Println("Blog Service Started")
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	opts := []grpc.ServerOption{}
+
+	s := grpc.NewServer(opts...)
+	blogpb.RegisterBlogServiceServer(s, &server{})
+
+	go func() {
+		fmt.Println("Starting Server..")
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+	// Wait for Control C to exit
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+
+	//Block until the signal is received
+	<-ch
+	fmt.Println("Stoppping the server")
+	s.Stop()
+	fmt.Println("Stopping the listener")
+	lis.Close()
+	fmt.Println("Closing Posgres DB")
+	db.Close()
+	fmt.Println("End of Program")
+
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module go-grpc
 go 1.16
 
 require (
+	github.com/lib/pq v1.10.4 // indirect
 	google.golang.org/grpc v1.42.0 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
 )
